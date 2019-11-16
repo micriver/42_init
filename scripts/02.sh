38b47dc34742454feff7deb5465883491c72227b
Michael Rivera <mirivera@student.42.us.org>
2019-11-15 16:06:34 -0800

    pushing user deletion script

--- scripts/02.sh
+++ scripts/02.sh
@@ -1,5 +1,12 @@
 # 02 [SCRIPT] Write a script which delete an ACTIVE user on the VM
-users | $USER 
-if [ $1 !root ]
-	userdel -f 
+users
+read -p "Which user? " user
+if [ $user != 'root' ] 
+then
+	userdel -f $user
+	echo "$user has been deleted."
+else
+	echo "the user is root!"
+fi
 
+# The -f (--force) flag will forcefully remove the user account, even if the user is still logged in or if there are running processes which belong to this user.
